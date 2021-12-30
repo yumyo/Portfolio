32a82d7bdb8099ff3eca97cb8c8428dcbfd6605d
Nicola Pernice <n.pernice@w2b.it>
2021-12-30 10:54:14 +0000

    Amend Contact For syntax

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,13 +6,13 @@ export default function ContactForm(props) {
   return (
     <form className={` ${props.className}`} method="POST" action={FORMSPARK_ACTION_URL}>
       {/* <h2 className="text-xxs leading-none uppercase mb-4 ">Contact</h2> */}
-      <label for="first-name" class="required">First name</label>
-      <input id="first-name" name="first-name" type="text" required />
-      <label for="last-name" class="required">Last name</label>
-      <input id="last-name" name="last-name" type="text" required />
-      <label for="email" class="required">Email</label>
+      <label htmlFor="first-name" className="required">First name</label>
+      <input id="first-name" className="first-name" type="text" required />
+      <label htmlFor="last-name" className="required">Last name</label>
+      <input id="last-name" className="last-name" type="text" required />
+      <label htmlFor="email" className="required">Email</label>
       <input type="email" name="email" required />
-      <label for="message" class="required">Message</label>
+      <label htmlFor="message" className="required">Message</label>
       <textarea name="message" placeholder="Your awesome message!" required />
       <button className="px-4 mt-4 leading-none py-3 border text-violet-600 dark:text-violet-400 border-violet-600 dark:border-violet-400 hover:bg-violet-600 hover:text-white dark:hover:bg-violet-400 dark:hover:text-black" type="submit">Send</button>
     </form>
