fba43c2c0fe06fe7da65015c12a47b6e49a3f6af
Giulia Nicola Pernice <npernice@dminc.com>
2024-08-27 18:21:24 +0100

    amend contact link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,33 @@
 import React from "react"
-import tw from "twin.macro"
 import TransitionLink from "gatsby-plugin-transition-link/AniLink"
-// import { RightPos, LeftPos, MenuLabel } from "../components/theme"
+import { gsap } from "gsap"
 
 export default function Footer(props) {
   const today = new Date()
   const year = today.getFullYear()
+
+  const exitAnimation = ({ node }) => {
+    return new Promise(resolve => {
+      gsap.to(node, {
+        autoAlpha: 0,
+        duration: 0.5,
+        ease: "sine.inOut",
+        onComplete: () => {
+          window.scrollTo(0, 0)  // Scroll to the top
+          resolve()
+        },
+      })
+    })
+  }
+
+  const entryAnimation = ({ node }) => {
+    gsap.from(node, {
+      autoAlpha: 0,
+      duration: 0.5,
+      ease: "sine.inOut",
+    })
+  }
+
   return (
     <div className="md:bottom static bottom-0 mx-auto flex w-k8 shrink-0 transform-gpu items-end justify-between pb-k1v leading-none md:z-50 md:pb-k1v xl:fixed lg:w-full">
       <div className={`static left-k1 z-40 flex lg:fixed`}>
@@ -28,15 +50,18 @@ export default function Footer(props) {
           to="/contact/"
           exit={{
             length: 0.5,
+            trigger: exitAnimation,
+            delay:0.5
           }}
-          entry={{ length: 0.5 }}
+          entry={{
+            length: 0.5,
+            trigger: entryAnimation,
+            delay:0.5
+          }}
           className=" "
           activeClassName=""
         >
           Contact
-          {/* <span className={`mt-1 mr-4 -mb-4 ${MenuLabel}`}>
-            Let's get in touch
-          </span> */}
         </TransitionLink>
       </div>
     </div>
